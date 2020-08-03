27284606f7d93b37a987c229d12345b50229a6b4
BenLeikin <69165113+BenLeikin@users.noreply.github.com>
2020-08-03 12:06:01 -0700

    Create EnvMonitor.sh

---
+++ EnvMonitor.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#cds to /home/pi/ where all of the scripts are located and then runs the python script. This is exclusively used in cron
+cd /
+cd /home/pi/
+sudo python3 EnvMonitor.py
