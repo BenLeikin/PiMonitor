7aa8050cb78b43af370a36aaceae40cd0876c01c
BenLeikin <69165113+BenLeikin@users.noreply.github.com>
2020-08-03 12:08:40 -0700

    Create SpeedTest.sh

---
+++ SpeedTest.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#runs the speedtest command from the speedtest-cli program. the --simple switch formats it into a simple format
+sudo /usr/local/bin/speedtest-cli --simple > SpeedTest.txt
